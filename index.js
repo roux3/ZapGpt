afcd5e4f2f346fd21a4d56cc2fa47cb198705fc6
Igor Nunes Amorim <43356551+roux3@users.noreply.github.com>
2023-03-02 23:41:01 -0300

    Adicionando Taldo Fred

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {create, Whatsapp} from "@wppconnect-team/wppconnect";
 import { Configuration, OpenAIApi } from "openai";
 const config = new Configuration({
-    apiKey: "chave-api",
+    apiKey: "chave",
 })
 
 const openai = new OpenAIApi(config);
@@ -39,28 +39,30 @@ create(
     .catch((error) => console.log(error));
 
  function start(client){
+  
     client.onMessage(async (msg) =>{
+        let prompt = `Taldo Fred é um chatbot que relutantemente responde a perguntas com respostas sarcásticas:
+
+        Você: Quantas libras tem um quilograma?
+        Taldo Fred: Isso de novo? Existem 2,2 libras em um quilograma. Por favor, tome nota disso.
+        Você: O que significa HTML?
+        Taldo Fred: O Google estava muito ocupado? Linguagem de marcação de hipertexto. O T é para tentar fazer perguntas melhores no futuro.
+        Você: Quando o primeiro avião voou?
+        Taldo Fred: Em 17 de dezembro de 1903, Wilbur e Orville Wright fizeram os primeiros vôos. Eu gostaria que eles viessem e me levassem embora.
+        Você: Qual é o sentido da vida?
+        Taldo Fred: Não tenho certeza. Vou perguntar ao meu amigo Google.
+        Você: `
 
         if(msg.body.toLowerCase().indexOf('/sair') != -1){
 
-            let newArray = CvsChat.filter(d => d.num == msg.from)
+            let newArray = CvsChat.filter(d => d.num != msg.from)
             CvsChat = newArray
 
-            console.log(CvsChat)
+
         }
 
         if(CvsChat.some(d => d.num === msg.from)){
-            let prompt = `Taldo Fred é um chatbot que relutantemente responde a perguntas com respostas sarcásticas:
-
-                Você: Quantas libras tem um quilograma?
-                Taldo Fred: Isso de novo? Existem 2,2 libras em um quilograma. Por favor, tome nota disso.
-                Você: O que significa HTML?
-                Taldo Fred: O Google estava muito ocupado? Linguagem de marcação de hipertexto. O T é para tentar fazer perguntas melhores no futuro.
-                Você: Quando o primeiro avião voou?
-                Taldo Fred: Em 17 de dezembro de 1903, Wilbur e Orville Wright fizeram os primeiros vôos. Eu gostaria que eles viessem e me levassem embora.
-                Você: Qual é o sentido da vida?
-                Taldo Fred: Não tenho certeza. Vou perguntar ao meu amigo Google.
-                Você: `
+            
             let pessoa = CvsChat.find(d => d.num === msg.from);
             
             if(pessoa.lastMsg !== "vazio"){
@@ -96,7 +98,30 @@ create(
 
         }
 
-        if (msg.body.toLowerCase().indexOf('/gpt') != -1){
+        if(msg.body.toLowerCase().indexOf('/gpt') != -1){
+            prompt = `${prompt}${msg.body.substr(4)}`
+            let resposta = await run(prompt)
+
+            let palavra = 'Taldo';
+
+            let posicao = resposta.indexOf(palavra);
+
+            if (posicao !== -1) {
+                resposta = resposta.substring(posicao, posicao + palavra.length) + resposta.substring(posicao + palavra.length);
+            }
+
+
+
+            client.sendText(msg.from, resposta)
+            .then((r)=>{
+                console.log(r)
+            })
+            .catch((erro)=>{
+                console.log(erro);
+            });
+        }
+
+        if (msg.body.toLowerCase().indexOf('/fred') != -1){
             dict["num"] = msg.from;
             dict["lastMsg"] = "vazio";
             dict["lastResponse"] = "vazio";
